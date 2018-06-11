91eb89f207950af161ea33f099df55f8ca4af2ba
Jake Rosen <jakesrosen@gmail.com>
2018-06-11 13:40:07 -0700

    Fixed undo functionality and cleaned up the code.

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
+{-# language GeneralizedNewtypeDeriving #-}
 {-# language ScopedTypeVariables #-}
 {-# language RecursiveDo #-}
+{-# language LambdaCase #-}
 
 module Main where
 
@@ -8,31 +10,9 @@ import Reactive.Banana.Frameworks
 -- import System.IO
 import qualified Graphics.Vty as Vty
 
-type Gold = Int
-
-data GameState = GameState
-  { gameGold :: Gold
-  , gameNumDrones :: Int
-  , gameGreen :: Int
-  , gameBlue :: Int
-  , gameRed :: Int
-  , gameEnergy :: Int
-  , gameDronesBuilding :: Int
-  } deriving Show
-
-data GameUnit
-  = Drone
-  | Engineer
-  | Conduit
-  | Blastforge
-  | Animus
-  deriving Show
-
-{-
-unitGoldCost :: GameUnit -> Gold
-unitGreenCost :: GameUnit -> Int
-unitEnergyCost :: GameUnit -> Int
--}
+-------------------------------------------------------------------------------
+-- Main - set up network and handle input
+-------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
@@ -138,6 +118,311 @@ main = do
   -- Feed inputs into network
   -- (Listen for key presses)
 
+-------------------------------------------------------------------------------
+-- Game logic - data types and pure functions
+-------------------------------------------------------------------------------
+
+newtype Gold = Gold Int
+  deriving (Show, Ord, Eq, Num)
+
+newtype Green = Green Int
+  deriving (Show, Ord, Eq, Num)
+
+newtype Red = Red Int
+  deriving (Show, Ord, Eq, Num)
+
+newtype Blue = Blue Int
+  deriving (Show, Ord, Eq, Num)
+
+newtype Energy = Energy Int
+  deriving (Show, Ord, Eq, Num)
+
+
+data GameState = GameState
+  { gameGold :: Gold
+  , gameNumDrones :: Int
+  , gameGreen :: Green
+  , gameBlue :: Blue
+  , gameRed :: Red
+  , gameEnergy :: Energy
+  , gameDronesBuilding :: Int
+  } deriving Show
+
+data Unit
+  = Animus
+  | Blastforge
+  | Conduit
+  | Drone
+  | Engineer
+  deriving (Show, Eq)
+
+unitGoldCost :: Unit -> Gold
+unitGoldCost = \case
+  Animus -> 6
+  Blastforge -> 5
+  Conduit -> 4
+  Drone -> 3
+  Engineer -> 2
+
+unitGreenCost :: Unit -> Green
+unitGreenCost = \case
+  Animus -> 0
+  Blastforge -> 0
+  Conduit -> 0
+  Drone -> 0
+  Engineer -> 0
+
+unitBlueCost :: Unit -> Blue
+unitBlueCost  = \case
+  Animus -> 0
+  Blastforge -> 0
+  Conduit -> 0
+  Drone -> 0
+  Engineer -> 0
+
+unitRedCost :: Unit -> Red
+unitRedCost = \case
+  Animus -> 0
+  Blastforge -> 0
+  Conduit -> 0
+  Drone -> 0
+  Engineer -> 0
+
+unitEnergyCost :: Unit -> Energy
+unitEnergyCost = \case
+  Animus -> 0
+  Blastforge -> 0
+  Conduit -> 0
+  Drone -> 1
+  Engineer -> 0
+
+canBuyUnit :: Unit -> Gold -> Green -> Blue -> Red -> Energy -> Bool
+canBuyUnit x g gr b r e = and
+  [ g >= unitGoldCost x
+  , gr >= unitGreenCost x
+  , b >= unitBlueCost x
+  , r >= unitRedCost x
+  , e >= unitEnergyCost x
+  ]
+
+unitGoldRate :: Unit -> Int
+unitGoldRate = \case
+  Animus -> 0
+  Blastforge -> 0
+  Conduit -> 0
+  Drone -> 1
+  Engineer -> 0
+
+unitGreenRate :: Unit -> Int
+unitGreenRate = \case
+  Animus -> 0
+  Blastforge -> 0
+  Conduit -> 1
+  Drone -> 0
+  Engineer -> 0
+
+unitBlueRate :: Unit -> Int
+unitBlueRate = \case
+  Animus -> 0
+  Blastforge -> 1
+  Conduit -> 0
+  Drone -> 0
+  Engineer -> 0
+
+unitRedRate :: Unit -> Int
+unitRedRate = \case
+  Animus -> 1
+  Blastforge -> 0
+  Conduit -> 0
+  Drone -> 0
+  Engineer -> 0
+
+-------------------------------------------------------------------------------
+-- Game logic - network
+-------------------------------------------------------------------------------
+
+prismataAssist :: Event Char -> MomentIO (Behavior GameState)
+prismataAssist eInput = mdo
+  -- Logic goes here
+  -- filterE :: (a -> Bool) -> Event a -> Event a 
+  (eNextTurn2 :: Event (), fireNextTurn2 :: () -> IO ()) <- 
+    newEvent
+
+  let
+    eNextTurn :: Event ()
+    eNextTurn = () <$ filterE (== '\n') eInput
+
+    eUndoTurn :: Event ()
+    eUndoTurn = () <$ filterE (== 'u') eInput
+
+    eBuyUnit :: Event Unit
+    eBuyUnit = filterApply bCanBuy (filterJust (charToUnit <$> eInput))
+      where
+        charToUnit :: Char -> Maybe Unit
+        charToUnit = \case
+          'a' -> Just Animus
+          'b' -> Just Blastforge
+          'c' -> Just Conduit
+          'd' -> Just Drone
+          'e' -> Just Engineer
+          _ -> Nothing
+
+    bCanBuy :: Behavior (Unit -> Bool)
+    bCanBuy =
+      pure (\gold green blue red energy unit -> 
+        canBuyUnit unit gold green blue red energy)
+      <*> bGold
+      <*> bGreen
+      <*> bBlue
+      <*> bRed
+      <*> bEnergy
+
+    -- (<$) ::          a -> Event b -> Event a
+    -- (<@) :: Behavior a -> Event b -> Event a
+
+  let 
+    undo :: (GameState -> a) -> Event (b -> a)
+    undo f = (\gs _ -> f gs) <$> bLastGameState <@ eUndoTurn
+
+  bNumDrones :: Behavior Int <-
+    accumB 6 
+      (unions
+        [ (+) <$> bDronesBuilding <@ eNextTurn
+        , undo gameNumDrones
+        ])
+
+  bDronesBuilding :: Behavior Int <- 
+    accumB 0
+      (unions
+        [ (\_ -> 0) <$ eNextTurn
+        , (\u g -> g + unitGoldRate u) <$> eBuyUnit
+        , undo gameDronesBuilding
+        ])
+
+  -- accumB :: Gold -> Event (Gold -> Gold) -> Moment (Behavior Gold)
+  bGold :: Behavior Gold <-
+    accumB 6 
+      (unions
+        [ (\x y z -> Gold (x + y) + z) <$> bNumDrones <*> bDronesBuilding
+                                <@ eNextTurn
+        , (\u g -> g - unitGoldCost u) <$> eBuyUnit
+        , undo gameGold
+        ])
+
+  bGreen :: Behavior Green <- do
+    bGreenRate :: Behavior Int <-
+      accumB 0 
+        (unions
+          [ (\u gr -> gr + unitGreenRate u) <$> eBuyUnit
+          ])
+
+    accumB 0
+      (unions
+        [ (\x y -> Green x + y) <$> bGreenRate <@ eNextTurn
+        , (\u gr -> gr - unitGreenCost u) <$> eBuyUnit
+        , undo gameGreen
+        ])
+
+  bBlue :: Behavior Blue <- do
+    bBlueRate :: Behavior Int <-
+      accumB 0 
+        (unions
+          [ (\u b -> b + unitBlueRate u) <$> eBuyUnit
+          ])
+        
+    accumB 0
+      (unions
+        [ (\n _ -> Blue n) <$> bBlueRate <@ eNextTurn
+        , (\u b -> b - unitBlueCost u) <$> eBuyUnit
+        , undo gameBlue
+        ])
+
+  bRed :: Behavior Red <- do
+    bRedRate :: Behavior Int <-
+      accumB 0 
+        (unions
+          [ (\u r -> r + unitRedRate u) <$> eBuyUnit
+          ])
+        
+    accumB 0
+      (unions
+        [ (\n _ -> Red n * 2) <$> bRedRate <@ eNextTurn
+        , (\u r -> r - unitRedCost u) <$> eBuyUnit
+        , undo gameRed
+        ])
+
+  bEnergy :: Behavior Energy <- do
+    bNumEngineers :: Behavior Int <- 
+      accumB 2
+        (unions
+          [ (+1) <$ filterE (==Engineer) eBuyUnit
+          ])
+
+    accumB 2 
+      (unions
+        [ (\n _ -> Energy n) <$> bNumEngineers <@ eNextTurn
+        , (\u e -> e - unitEnergyCost u) <$> eBuyUnit
+        , undo gameEnergy
+        ])
+
+  {-
+  eFoo
+  bBar
+  bBaz
+       f                            :: Bar -> Baz -> Foo -> Whatever
+  pure f                            :: Behavior (Bar -> Baz -> Foo -> Whatever)
+  pure f <*> bBar                   :: Behavior (Baz -> Foo -> Whatever)
+  pure f <*> bBar <*> bBaz          :: Behavior (Foo -> Whatever)
+  pure f <*> bBar <*> bBaz <@> eFoo :: Event Whatever
+  -}
+
+
+  --                        *----------
+  --
+  --
+  --            ------------*
+  --
+  --
+  -- -----------*
+  --
+  --                        ^               ^
+  --                        ^
+
+  -- Time-varying entire game state
+  bLastGameState :: Behavior GameState <- do
+    gs <- valueBLater bGameState
+    stepper gs (bGameState <@ eNextTurn2)
+    -- (valueB bGameState0) >>= (flip stepper (bGameState <@ eNextTurn))
+
+  let
+    bGameState :: Behavior GameState
+    bGameState = pure GameState 
+                   <*> bGold 
+                   <*> bNumDrones 
+                   <*> bGreen
+                   <*> bBlue
+                   <*> bRed
+                   <*> bEnergy
+                   <*> bDronesBuilding
+
+  reactimate (fireNextTurn2 <$> eNextTurn)
+
+  {-
+  bGameState :: Behavior GameState <-
+    switchB bGameState0 ((bLastGameState <$ eUndoTurn))
+  -}
+  pure bGameState
+
+
+
+
+
+
+
+-------------------------------------------------------------------------------
+-- Rendering
+-------------------------------------------------------------------------------
+
 renderGame :: GameState -> Vty.Image
 renderGame gs = 
   str ("Drones: " ++ show (gameNumDrones gs)
@@ -168,164 +453,8 @@ infixr 4 +-+
 (+|+) = (Vty.<|>)
 infixr 5 +|+
 
-prismataAssist :: MonadMoment m => Event Char -> m (Behavior GameState)
-prismataAssist eInput = mdo
-  -- Logic goes here
-  -- filterE :: (a -> Bool) -> Event a -> Event a 
-  let
-    eNextTurn :: Event ()
-    eNextTurn = () <$ filterE (== '\n') eInput
 
-    eUndoTurn :: Event ()
-    eUndoTurn = () <$ filterE (== 'u') eInput
 
-    eBuyDrone :: Event ()
-    eBuyDrone = 
-      whenE bCanBuyDrone (() <$ filterE (== 'd') eInput)
-     where
-      bCanBuyDrone :: Behavior Bool
-      bCanBuyDrone = pure (\g e -> g >= 3 && e >= 1)
-                        <*> bGold
-                        <*> bEnergy
 
-    -- (<$) ::          a -> Event b -> Event a
-    -- (<@) :: Behavior a -> Event b -> Event a
 
-  bNumDrones :: Behavior Int <-
-    accumB 6 
-      (unions
-        [ (+) <$> bDronesBuilding <@ eNextTurn
-        ])
 
-  bDronesBuilding :: Behavior Int <- 
-    accumB 0
-      (unions
-        [ (\_ -> 0) <$ eNextTurn
-        , (+1) <$ eBuyDrone
-        ])
-
-  -- accumB :: Gold -> Event (Gold -> Gold) -> Moment (Behavior Gold)
-  bGold :: Behavior Gold <-
-    accumB 6 
-      (unions
-        [ (\x y z -> x + y + z) <$> bNumDrones <*> bDronesBuilding
-                                <@ eNextTurn
-        , subtract 3 <$ eBuyDrone
-        , subtract 2 <$ eBuyEngineer
-        , subtract 4 <$ eBuyConduit
-        , subtract 5 <$ eBuyBlastforge
-        , subtract 6 <$ eBuyAnimus
-        ])
-
-  let 
-    eBuyConduit :: Event ()
-    eBuyConduit = 
-      whenE bCanBuyConduit (() <$ filterE (== 'c') eInput)
-     where
-      bCanBuyConduit :: Behavior Bool
-      bCanBuyConduit = (>= 4) <$> bGold
-
-  bGreen :: Behavior Int <- do
-    bNumConduit :: Behavior Int <-
-      accumB 0 
-        (unions
-          [ (+1) <$ eBuyConduit
-          ])
-
-    accumB 0
-      (unions
-        [ (+) <$> bNumConduit <@ eNextTurn
-        ])
-
-  let 
-    eBuyBlastforge :: Event ()
-    eBuyBlastforge = 
-      whenE bCanBuyBlastforge (() <$ filterE (== 'b') eInput)
-     where
-      bCanBuyBlastforge :: Behavior Bool
-      bCanBuyBlastforge = (>= 5) <$> bGold
-
-  bBlue :: Behavior Int <- do
-    bNumBlastforge :: Behavior Int <-
-      accumB 0 
-        (unions
-          [ (+1) <$ eBuyBlastforge
-          ])
-        
-    accumB 0
-      (unions
-        [ (\n _ -> n) <$> bNumBlastforge <@ eNextTurn
-        ])
-
-  let 
-    eBuyAnimus :: Event ()
-    eBuyAnimus = 
-      whenE bCanBuyAnimus (() <$ filterE (== 'a') eInput)
-     where
-      bCanBuyAnimus :: Behavior Bool
-      bCanBuyAnimus = (>= 6) <$> bGold
-
-  bRed :: Behavior Int <- do
-    bNumAnimus :: Behavior Int <-
-      accumB 0 
-        (unions
-          [ (+1) <$ eBuyAnimus
-          ])
-        
-    accumB 0
-      (unions
-        [ (\n _ -> n * 2) <$> bNumAnimus <@ eNextTurn
-        ])
-
-  let 
-    eBuyEngineer :: Event ()
-    eBuyEngineer = 
-      whenE bCanBuyEngineer (() <$ filterE (== 'e') eInput)
-     where
-      bCanBuyEngineer :: Behavior Bool
-      bCanBuyEngineer = (>= 2) <$> bGold
-
-  bEnergy :: Behavior Int <- do
-    bNumEngineers :: Behavior Int <- 
-      accumB 2
-        (unions
-          [ ((+1) <$ eBuyEngineer)
-          ])
-
-    accumB 2 
-      (unions
-        [ (\n _ -> n) <$> bNumEngineers <@ eNextTurn
-        , subtract 1 <$ eBuyDrone
-        ])
-
-  {-
-  eFoo
-  bBar
-  bBaz
-       f                            :: Bar -> Baz -> Foo -> Whatever
-  pure f                            :: Behavior (Bar -> Baz -> Foo -> Whatever)
-  pure f <*> bBar                   :: Behavior (Baz -> Foo -> Whatever)
-  pure f <*> bBar <*> bBaz          :: Behavior (Foo -> Whatever)
-  pure f <*> bBar <*> bBaz <@> eFoo :: Event Whatever
-  -}
-
-  -- Time-varying entire game state
-  bLastGameState :: Behavior GameState <- do
-    gs <- valueBLater bGameState0
-    stepper gs (bGameState <@ eNextTurn)
-    -- (valueB bGameState0) >>= (flip stepper (bGameState <@ eNextTurn))
-
-  let
-    bGameState0 :: Behavior GameState
-    bGameState0 = pure GameState 
-                   <*> bGold 
-                   <*> bNumDrones 
-                   <*> bGreen
-                   <*> bBlue
-                   <*> bRed
-                   <*> bEnergy
-                   <*> bDronesBuilding
-
-  bGameState :: Behavior GameState <-
-    switchB bGameState0 ((bLastGameState <$ eUndoTurn))
-  pure bGameState
